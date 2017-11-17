26204272141d9fbfccd7f4c65ec5da498fb8949d
ntftrader <ntftrader@gmail.com>
2017-11-17 19:48:56 +0100

    Added setup.py to project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ntfdl',
+      version='0.0.1',
+      description='Download free financial data from Netfonds ASA',
+      url='http://github.com/ntftrader/ntfdl',
+      author='Ntftrader',
+      author_email='ntftrader@gmail.com',
+      license='MIT',
+      packages=['ntfdl'],
+      zip_safe=False)
